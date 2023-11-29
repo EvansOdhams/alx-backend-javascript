941adc4046e040fa993c5101dbce5b1bd194730b
EvansOdhams <evansodhams@gmail.com>
2023-11-29 23:10:03 +0300

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,28 +1,13 @@
-// 6-final-user.js
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
-
-async function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  try {
-    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
-
-    const userStatus = userResult.status === 'fulfilled' ? 'fulfilled' : 'rejected';
-    const photoStatus = photoResult.status === 'fulfilled' ? 'fulfilled' : 'rejected';
-
-    const userValue = userResult.status === 'fulfilled' ? userResult.value : userResult.reason;
-    const photoValue = photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason;
-
-    return [
-      { status: userStatus, value: userValue },
-      { status: photoStatus, value: photoValue }
-    ];
-  } catch (error) {
-    return [{ status: 'rejected', value: error }];
-  }
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
-
-export default handleProfileSignup;
