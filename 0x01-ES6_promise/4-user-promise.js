9ea543d11089ef409737f0da23115ed6ec6299fd
EvansOdhams <evansodhams@gmail.com>
2023-11-29 23:06:25 +0300

    4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,12 +1,3 @@
-// 4-user-promise.js
-
-function signUpUser(firstName, lastName) {
-  return new Promise((resolve) => {
-    resolve({
-      firstName: firstName,
-      lastName: lastName
-    });
-  });
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
 }
-
-export default signUpUser;
